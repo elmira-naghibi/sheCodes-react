d91045e0318b63c3c87a582f6a38d8cf094cd1ab
Elmira Naghibi <elmiranaghibi@gmail.com>
2023-09-08 19:45:52 +0330

    feat: add space

--- src/Weather.js
+++ src/Weather.js
@@ -73,7 +73,7 @@ export default function Weather(props) {
           city={weatherData.city}
         />
         <footer>
-          This project was coded by Elmira Naghibi in
+          This project was coded by Elmira Naghibi in{" "}
           <a
             href="https://www.shecodes.io/"
             target="_blank"
